536c7bd30cb08e269688f9bb72339ab10d76f15b
Indra Prastha <iprastha@gmail.com>
2015-08-06 00:25:47 +0700

    Fix template error in title

--- application/views/template/default.php
+++ application/views/template/default.php
@@ -2,14 +2,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php echo $content['title'] ; ?></title>
-	<link rel="stylesheet" href="">
+	<title><?php echo content('title','Untitled') ; ?></title>
 </head>
 <body>
-	<?php echo content('body') ; ?>
+	<?php echo content('body', 'No Content') ; ?>
 	<hr>
-	<?php echo content('footer', 'Footer did not exists, this is the default footer text') ; ?>
+	<?php echo content('footer', 'If footer block is not provided, this is the default footer text') ; ?>
 </body>
 </html>
